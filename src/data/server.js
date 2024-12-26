7399c33f11841a445d0aac760cf91bc01e6b2cb5
Cahune <142368545+Cahune@users.noreply.github.com>
2024-12-27 02:10:58 +0700

    Update server.js

--- src/data/server.js
+++ src/data/server.js
@@ -6,9 +6,9 @@ const dotenv = require('dotenv');
 // Import các route
 const placeRoutes = require('./place');
 const taxiRoutes = require('./taxis');
-// const commentRoutes = require('./comments');
-// const hotelRoutes = require('./hotels');
-// const authRoutes = require('./signup-login');
+const commentRoutes = require('./comments');
+const hotelRoutes = require('./hotels');
+const authRoutes = require('./signup-login');
 
 const app = express();
 
@@ -36,9 +36,9 @@ mongoose
 // Đăng ký các route
 app.use('/api/place', placeRoutes);  // Các route cho "place"
 app.use('/api/taxis', taxiRoutes);  // Các route cho "taxis"
-// app.use('/api/comments', commentRoutes);  // Các route cho "comments"
-// app.use('/api/hotels', hotelRoutes);  // Các route cho "hotels"
-// app.use('/api/auth', authRoutes);  // Các route cho "signup-login"
+app.use('/api/comments', commentRoutes);  // Các route cho "comments"
+app.use('/api/hotels', hotelRoutes);  // Các route cho "hotels"
+app.use('/api/auth', authRoutes);  // Các route cho "signup-login"
 
 // Lắng nghe trên cổng Render cấp hoặc cổng mặc định
 const PORT = process.env.PORT;
