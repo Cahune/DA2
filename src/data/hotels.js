aeb3298f335aa7ad1bbb026e2a04fb9eba793a0b
Cahune <142368545+Cahune@users.noreply.github.com>
2024-12-27 03:14:29 +0700

    Update hotels.js

--- src/data/hotels.js
+++ src/data/hotels.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const { Hotel } = require("../config/config"); 
 
 router.get('/:placeId', async (req, res) => {
+    const { placeId } = req.params;
     try {
-        const { placeId } = req.params;
-        
-        const ObjectId = mongoose.Types.ObjectId;
-        const hotels = await Hotel.find({ places: new ObjectId(placeId) });
-
+        const hotels = await Hotel.find({ places: placeId });
         if (hotels.length === 0) {
           return res.status(404).json({ message: "Không tìm thấy khách sạn nào cho địa điểm này." });
         }
 
-        res.status(200).json(hotels); 
+        res.json(hotels); 
     } catch (error) {
         console.error("Lỗi khi lấy thông tin khách sạn:", error);
         res.status(500).json({ error: "Có lỗi xảy ra khi lấy thông tin khách sạn." });
